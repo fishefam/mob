49181da4ae6f8ad7dd818e72eb9cc1bcce0bd7a3
mnguyen <mnguyen@evenica.com>
2024-05-04 14:01:10 -0400

    .

--- scripts/pre-commit.ts
+++ scripts/pre-commit.ts
@@ -1,5 +1,6 @@
 import { spawnSync } from 'child_process'
 import { buildSync } from 'esbuild'
+import { resolve } from 'path'
 
 import { getDirs } from './libs/constants'
 import { resolveRelative } from './libs/utils'
@@ -8,13 +9,14 @@ main()
 
 function main() {
   const { nodeModules, scripts } = getDirs()
-  const lintStaged = { cmd: <BinCmds>'lint-staged', out: <BinCmds>'lint-staged' + '.config' }
+  const lintStaged = { cmd: <BinCmds>'lint-staged', out: resolve(nodeModules, <BinCmds>'lint-staged' + '.config') }
   buildSync({
     bundle: true,
     entryPoints: [{ in: resolveRelative(scripts, 'lint-staged.ts'), out: lintStaged.out }],
     external: ['micromatch'],
     format: 'cjs',
+    outdir: nodeModules,
     platform: 'node',
   })
-  spawnSync(`${lintStaged.cmd} --config ${resolveRelative(nodeModules, lintStaged.out)}`)
+  spawnSync(`${lintStaged.cmd} --config ${lintStaged.out}`)
 }
